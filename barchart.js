e02e09b0b8d99dafeb010fe8d09ec5d7fa226f98
Alejandro Cristoffanini <acristoffanini@gmail.com>
2021-09-07 11:34:39 -0700

    added some comments, testing updates to github pages

--- barchart.js
+++ barchart.js
@@ -1,6 +1,11 @@
+//drawbar chart parameters is as follows, data is an array of numerical values, options is an array of strings, and element is the DOM/jQuery element that will render the chart
+//data array goes [(data value1(data value subset1)), (data value2(data value subset2) ...) ]
+//options array goes["numerical value position", "bar colour", "label colour", "bar spacing (pass this as a number?)", "bar chart axis-X name", "bar chart title", "bar chart title font", "bar chart title size" ]
 function drawBarChart(data, options, element) {
 
 };
+
+
 
 //input
 console.log(drawBarChart())
